c64e31f42a66a80761051cfdfac15781002e478e
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2025-06-12 18:51:01 +0700

    membuat swich ch

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -3,109 +3,109 @@ using namespace std;
 
 class Queues
 {
-    private:
-          static const int max = 5;
-          int front, rear;
-          int queue_array[5];
+private:
+    static const int max = 5;
+    int front, rear;
+    int queue_array[5];
 
-    public:
-        Queues()
+public:
+    Queues()
+    {
+        front = -1;
+        rear = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "enter a number :";
+        cin >> num;
+        cout << endl;
+
+        if ((front == 0 && rear == max - 1) || (front == rear + 1))
+        {
+            cout << "\nQueue overvlow\n";
+            return;
+        }
+
+        if (front == -1)
+        {
+            front = 0;
+            rear = 0;
+        }
+        else
+        {
+            if (rear == max - 1)
+                rear = 0;
+            else
+                rear = rear + 1;
+        }
+        queue_array[rear] = num;
+    }
+
+    void remove()
+    {
+        if (front == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queue is :" << queue_array[front] << "\n";
+
+        if (front == rear)
         {
             front = -1;
             rear = -1;
         }
-
-        void insert()
+        else
         {
-            int num;
-            cout << "enter a number :";
-            cin >> num;
+            if (front == max - 1)
+                front = 0;
+            else
+                front = front + 1;
+        }
+    }
+
+    void display()
+    {
+        int front_position = front;
+        int rear_position = rear;
+
+        if (front == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+        cout << "\nElelments in the queue are....\n";
+
+        if (front_position <= rear_position)
+        {
+            while (front_position <= rear_position)
+            {
+                cout << queue_array[front_position] << "  ";
+                front_position++;
+            }
             cout << endl;
-
-            if ((front == 0 && rear == max -1) || (front == rear +1))
-            {
-                cout << "\nQueue overvlow\n";
-                return;
-            }
-
-            if (front == -1)
-            {
-               front = 0;
-               rear = 0;
-            }
-            else
-            {
-                if(rear == max -1)
-                rear = 0;
-                else
-                   rear = rear +1;
-            }
-            queue_array[rear] = num;
         }
-
-        void remove()
+        else
         {
-            if (front == -1)
+            while (front_position <= max - 1)
             {
-                cout << "Queue underflow\n";
-                return;
+                cout << queue_array[front_position] << "  ";
+                front_position++;
             }
-            cout << "\nThe element deleted from the queue is :" <<queue_array[front] << "\n";
 
-            if (front == rear)
+            front_position = 0;
+
+            while (front_position <= rear_position)
             {
-                front = -1;
-                rear = -1;
+                cout << queue_array[front_position] << "  ";
+                front_position++;
             }
-            else
-            {
-                if(front == max -1)
-                   front = 0;
-                else 
-                 front = front +1;
-            }
+
+            cout << endl;
         }
-
-        void display()
-        {
-            int front_position = front;
-            int rear_position = rear;
-
-            if (front == -1)
-            {
-                cout << "Queue is empty\n";
-                return;
-            }
-            cout << "\nElelments in the queue are....\n";
-
-            if (front_position <= rear_position)
-            {
-                while (front_position <= rear_position)
-                {
-                    cout << queue_array[front_position] << "  ";
-                    front_position++;
-                }
-                cout << endl;
-            }
-            else
-            {
-                while (front_position <= max -1)
-                {
-                    cout << queue_array[front_position] << "  ";
-                    front_position++;
-                }
-
-                front_position = 0;
-
-                while (front_position <= rear_position)
-                {
-                    cout << queue_array[front_position] << "  ";
-                    front_position++;
-                }
-                
-                cout << endl;
-            }
-        }
+    }
 };
 
 int main()
@@ -125,12 +125,39 @@ int main()
             cout << "enter your choise (1-4) : ";
             cin >> ch;
             cout << endl;
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << e.what() << '\n';
-        }
+
+            switch (ch)
+            {
+            case '1':
+            {
+
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+
+            default:
+            {
+                cout << "invalid option !!" << endl;
+                break;
+            }
         
+            }
+        }
     }
-    
 }
