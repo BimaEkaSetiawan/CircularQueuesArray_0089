f86069a37e11692bf46a72b423cd3ac737df608e
BimaEkaSetiawan <bimaekasetiawan1010@gmail.com>
2025-06-12 17:50:55 +0700

    meremove anrtian

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -40,6 +40,29 @@ class Queues
                 else
                    rear = rear +1;
             }
-            
+            queue_array[rear] = num;
+        }
+
+        void remove()
+        {
+            if (front == -1)
+            {
+                cout << "Queue underflow\n";
+                return;
+            }
+            cout << "\nThe element deleted from the queue is :" <<queue_array[front] << "\n";
+
+            if (front == rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            else
+            {
+                if(front == max -1)
+                   front = 0;
+                else 
+                 front = front +1;
+            }
         }
 };
